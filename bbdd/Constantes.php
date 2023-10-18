4e8db99704bd0acd8d5d4f1fac1da6d1aace3d2d
David Trujillo Carrero <davitru60@gmail.com>
2023-10-18 18:20:13 +0200

    modificacion

--- bbdd/Constantes.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Constantes{
-    static $SERVIDOR="localhost";
-    static $USUARIO="root";
-    static $CLAVE="";
-    static $BBDD="buscaminas";
-    static $TAM_TABLERO=20;
-    static $MINAS=3;
-}
